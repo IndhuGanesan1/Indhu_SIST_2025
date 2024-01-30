02063fbd5e26cd6b7ac5a3285c3d8bfa94317751
IndhuGanesan1 <157341353+IndhuGanesan1@users.noreply.github.com>
2024-01-30 07:07:05 +0530

    Create Q14_Anagram.py

---
+++ 27.01.24/Q14_Anagram.py
@@ -0,0 +1,62 @@
+#Two words are anagrams of one another if their letters can be rearranged to form the other word.
+
+#Given a string, split it into two contiguous substrings of equal length. Determine the minimum number of characters to change to make the two substrings into anagrams of one another.
+
+#Example
+
+#Break  into two parts: 'abc' and 'cde'. Note that all letters have been used, the substrings are contiguous and their lengths are equal. Now you can change 'a' and 'b' in the first substring to 'd' and 'e' to have 'dec' and 'cde' which are anagrams. Two changes were necessary.
+
+#Function Description
+
+#Complete the anagram function in the editor below.
+
+#anagram has the following parameter(s):
+
+#string s: a string
+#Returns
+
+#int: the minimum number of characters to change or -1.
+#Input Format
+
+#The first line will contain an integer, , the number of test cases.
+#Each test case will contain a string .
+
+#Constraints
+
+
+# consists only of characters in the range ascii[a-z].
+#Sample Input
+
+#6
+#aaabbb
+#ab
+#abc
+#mnop
+#xyyx
+#xaxbbbxx
+#Sample Output
+
+#3
+#1
+#-1
+#2
+#0
+#1
+T = int(input())
+for i in range(0,T):
+	al= (input())
+	l = len(al)
+	if (l%2==0):
+		half = int(l/2)
+		a = al[:half]
+		b = al[half:]
+		count = 0
+		for c in a:
+			if c in b:
+				pos = b.find(c)
+				b=b[:pos]+b[pos+1:]
+			else:
+			 count += 1
+		print (count)
+	else:
+		print (-1)
